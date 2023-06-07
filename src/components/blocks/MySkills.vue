994263424ea1a996e8b4ac9e658b7503b0615469
snowfallen <bondarenkoov1998@gmail.com>
2023-06-07 17:20:07 +0200

    add new block

--- src/components/blocks/MySkills.vue
+++ src/components/blocks/MySkills.vue
@@ -1,13 +1,34 @@
 <template>
-
+  <div id="skills">
+    <div class="ball" ref="ball1Ref" :style="{ top: ball1Top + 'px', left: ball1Left + 'px' }">PHP</div>
+    <div class="ball" ref="ball2Ref" :style="{ top: ball2Top + 'px', left: ball2Left + 'px' }">Laravel</div>
+  </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: "MySkills"
-}
+import { defineComponent} from "vue";
+
+export default defineComponent({
+  name: "MySkills",
+})
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+#skills {
+  position: relative;
+  width: 500px;
+  height: 500px;
+  border: 1px solid black;
 
+  .ball {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
